b728bb095ed11b2156f442027f2fa123536f6bfd
劉敦桐 <zse811022@hotmail.com>
2023-05-13 15:32:05 +0800

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Actor
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        public string ProfilePictureURL { get;set;}
+
+        public string FullName { get; set;}
+
+        public string Bio { get; set;}
+
+
+    }
+}
